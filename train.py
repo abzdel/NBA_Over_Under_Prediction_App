38bad4ee4f3f9f12aec4ee27b445fbdf4c21e2ca
Alex Bzdel <ab913@duke.edu>
2022-12-13 00:45:08 +0000

    working on bringing in current game for prediction

--- train.py
+++ train.py
@@ -9,9 +9,10 @@ print("Training models...")
 team1_stats = pd.read_csv("data/team1_stats.csv")
 team2_stats = pd.read_csv("data/team2_stats.csv")
 
+features = ["Rk", "Chg", "Home"]
 
 # train and export team 1 model
-X = team1_stats.drop(columns={"Tm"})
+X = team1_stats[features]
 y = team1_stats["Tm"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -23,7 +24,7 @@ joblib.dump(model, 'models/team1_model.pkl')
 print("Team 1 model trained and saved.")
 
 # train and export team 2 model
-X = team2_stats.drop(columns={"Tm"})
+X = team2_stats[features]
 y = team2_stats["Tm"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+import datetime
 
 # data cleaning functions
 def home_to_binary(home):
@@ -23,6 +24,10 @@ def get_day_of_week(date):
     """function to return day of week (first 3 letters) from date"""
     return date[:3]
 
+def get_date(date):
+    """truncates day of week out of column, returns datetime object"""
+    return datetime.datetime.strptime(date[4:], "%b %d, %Y")
+
 # load data, predict, and return results
 def load_data(team_num):
     df = pd.read_csv(f"data/team{team_num}_stats.csv")
@@ -32,16 +37,30 @@ def train(df):
     # run train.py file
     os.system("python train.py")
 
-def get_matchup_to_predict():
-    # get matchup to predict
-    matchup = pd.read_csv("matchup.txt", header=None)
-    matchup = str(matchup[0].values[0])
+def get_matchup_to_predict(team1, team2):
+    # scrape data once again
+    # will be merged a little differently than before
+    team1_stats = pd.read_html(f"https://www.basketball-reference.com/teams/{team1}/2023_games.html")[0]
+    team2_stats = pd.read_html(f"https://www.basketball-reference.com/teams/{team2}/2023_games.html")[0]
 
-    # split matchup into team1 and team2
-    team1 = matchup.split(" ")[0]
-    team2 = matchup.split(" ")[-1]
+    # get date function
+    team1_stats["Date"] = team1_stats["Date"].apply(get_date)
+    team2_stats["Date"] = team2_stats["Date"].apply(get_date)
 
-    return team1, team2
+    # ensure games are from today
+    today = datetime.today().strftime("%b %d, %Y")
+    team1_stats = team1_stats[team1_stats["Date"] == today]
+    team2_stats = team2_stats[team2_stats["Date"] == today]
+
+    if not today:
+        print("No games today.")
+        exit(0)
+
+    ranks = pd.read_csv("data/ranks.csv")
+
+    team1_stats = team1_stats.merge(ranks, how="left", left_on="Date", right_on="Date")
+
+
 
 def predict(team_num, df):
     # load model
